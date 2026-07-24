b98fd4a0e1a5497e15d86197e480430ae5829c05
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-07-23 18:28:38 -0600

    feat(seo): dynamically sync index.html title and OpenGraph meta tags with couple names

--- src/InvitationApp.tsx
+++ src/InvitationApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
 
 import { RouterApp } from '@/router'
@@ -8,16 +8,31 @@ import { ModalMaster } from '@/common/components/modal/ModalMaster'
 import { ToastContainer } from '@/common/components/toast/ToastContainer'
 import { Menu } from '@/common/components/menu/Menu'
 import { MusicPlayer } from '@/common/components/music-player/MusicPlayer'
+import { useInvitationConfig } from '@/common/hooks'
 
-export const InvitationApp: React.FC = () => {
+const InvitationAppContent: React.FC = () => {
+  const { sections, theme } = useInvitationConfig()
+
+  useEffect(() => {
+    const coupleNames = sections.hero?.names || theme.menu?.title || 'Invitación Especial'
+    document.title = `${coupleNames} | Invitación Especial`
+  }, [sections, theme])
+
   return (
-    <Provider store={store}>
+    <>
       <RouterApp />
-
       <ModalMaster />
       <ToastContainer />
       <Menu />
       <MusicPlayer />
+    </>
+  )
+}
+
+export const InvitationApp: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <InvitationAppContent />
     </Provider>
   )
 }
